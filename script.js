e79bcd72a34d82228bb4c011ea843f08f629f35c
jmorales113 <jmorales113@gmail.com>
2020-06-22 14:12:09 -0700

    Created eight-ball and array of answers

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log("You got this Jorge!");
+const answers = [
+  "It is certain",
+  "It is decidedly so",
+  "Without a doubt",
+  "Yes - definitely",
+  "You may rely on it",
+  "As I see it, yes",
+  "Most likely",
+  "Outlook good",
+  "Yes, signs point to yes",
+  "Don't count on it",
+  "My reply is no",
+  "My sources say no",
+  "Outlook not so good",
+  "Very doubtful",
+  "Reply hazy, try again",
+  "Ask again later",
+  "Better not tell you now",
+  "Cannot predict now",
+  "Concentrate and ask again",
+];
+
+document.onload = () => {
+  const question = document.getElementById("question");
+  const eightBall = document.getElementById("eight-ball");
+  const eight = document.getElementById("eight");
+  const answer = document.getElementById("answer");
+  console.log("hello");
+};
