c21552f933a3893e4a03cabf65b82ba351268bf6
jmorales113 <jmorales113@gmail.com>
2020-06-22 14:52:03 -0700

    Added event listener

--- script.js
+++ script.js
@@ -1,29 +1,33 @@
-const answers = [
-  "It is certain",
-  "It is decidedly so",
-  "Without a doubt",
-  "Yes - definitely",
-  "You may rely on it",
-  "As I see it, yes",
-  "Most likely",
-  "Outlook good",
-  "Yes, signs point to yes",
-  "Don't count on it",
-  "My reply is no",
-  "My sources say no",
-  "Outlook not so good",
-  "Very doubtful",
-  "Reply hazy, try again",
-  "Ask again later",
-  "Better not tell you now",
-  "Cannot predict now",
-  "Concentrate and ask again",
-];
-
-document.onload = () => {
+window.onload = function () {
   const question = document.getElementById("question");
   const eightBall = document.getElementById("eight-ball");
   const eight = document.getElementById("eight");
   const answer = document.getElementById("answer");
-  console.log("hello");
+  const answers = [
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes - definitely",
+    "You may rely on it",
+    "As I see it, yes",
+    "Most likely",
+    "Outlook good",
+    "Yes, signs point to yes",
+    "Don't count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful",
+    "Reply hazy, try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+  ];
+
+  eightBall.addEventListener("click", () => {
+    if (question.value.length < 1) {
+      alert("Please enter a question");
+    }
+  });
 };
